79624616e21ee0538ecc157ef4f279664b847847
OscarKarlsson <70796269+OscarKarlsson@users.noreply.github.com>
2022-11-19 07:39:41 +0100

    Add project files.

---
+++ ConsoleApp2/DataManagement/Randomizer.cs
@@ -0,0 +1,27 @@
+﻿using ConsoleApp2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp2.DataManagement
+{
+    internal class Randomizer
+    {
+        private Random rng = new Random();
+
+        internal List<Person> personList(List<Person> persons)
+        {
+            List<Person> personRandom = new List<Person>();
+
+            while (persons.Count() >= 0)
+            {
+                int rnd = rng.Next(persons.Count());
+                personRandom.Add(persons[rnd]);
+                persons.RemoveAt(rnd);
+            }
+
+            return personRandom;
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using ConsoleApp2.DataManagement;
+using ConsoleApp2.Models;
+
+Randomizer randomizer = new Randomizer();
+
+Console.WriteLine("Insert Credentials for sender email\nEmail:");
+string senderEmail = Console.ReadLine();
+Console.WriteLine("Password:");
+string senderPassword = Console.ReadLine();
+
+List<Person> people = new List<Person>();
+List<Person> peopleRnd = new List<Person>();
+
+bool newPerson = true;
+
+while (newPerson)
+{
+    Console.WriteLine("Insert name of person:");
+    string personName = Console.ReadLine();
+    Console.WriteLine("Insert their Email:");
+    string personEmail = Console.ReadLine();
+    people.Add(new Person { Name = personName, Email = personEmail });
+    Console.WriteLine("Would you like to add another person? (y/n)");
+    if (Console.ReadLine().ToUpper() == "N")
+    {
+        newPerson = false;
+    }
+}
+
+peopleRnd = randomizer.personList(people);
+
+Console.WriteLine(peopleRnd[1]);
